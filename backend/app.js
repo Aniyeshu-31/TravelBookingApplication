8fabefac6cd2a656058449bf169d203ecd2c4da4
Aniyeshu <202152304@iiit.vadodara.ac.in>
2024-11-21 20:25:17 +0530

    updated Backend

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const corsOption = {
 }
 // Allow frontend at localhost:3000
 const corsOptions = {
-  origin: 'http://localhost:3000', // Allow requests from this origin
+  origin: 'https://backend-travel-app.onrender.com', // Allow requests from this origin
   credentials: true, // Allow credentials (cookies, etc.) if needed
 }
 app.use(cors(corsOptions))
