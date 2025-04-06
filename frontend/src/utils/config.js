0379a334260ded991ca1d5444556a83160beae0a
Aniyeshu <202152304@iiit.vadodara.ac.in>
2025-04-07 00:18:22 +0530

    updatedFrontendURL

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
 // export const BASE_URL= 'http://localhost:4000/api/v1'
 export const RAZORPAY_KEY_ID = 'rzp_test_tPle5nA3lXrAtL'
-export const BASE_URL = 'https://backend-travel-app.onrender.com/api/v1'
+export const BASE_URL = 'https://travelapplicationbackend.onrender.com/api/v1'
