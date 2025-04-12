6b54f69972f3feea4a32241a36e71505382c1de8
Aniyeshu <202152304@iiit.vadodara.ac.in>
2025-04-12 21:30:36 +0530

    updated design

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React from 'react'
 
-import Header from "./../Header/Header";
-import Routers from "../../router/Routers";
-import Footer from "./../Footer/Footer";
+import Header from './../Header/Header'
+import Routers from '../../router/Routers'
+import Footer from './../Footer/Footer'
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Routers />
+      <div className="page-container">
+        <Routers />
+      </div>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
